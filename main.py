13685bc99af2262f706918552ae78a18f86c377f
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2024-11-07 10:35:03 +0100

    Add 'create storage file if it doesn't exist' function to the main.py file

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
 import argparse
 import os
+import json
+import csv
 from movie_app import MovieApp, StorageCsv, StorageJson
+
+
+def create_file_if_not_exists(storage_file):
+    """Create an empty storage file if it doesn't exist."""
+    if not os.path.exists(storage_file):
+        _, file_extension = os.path.splitext(storage_file)
+        if file_extension == ".json":
+            with open(storage_file, "w") as file:
+                json.dump({}, file)
+        elif file_extension == ".csv":
+            with open(storage_file, "w", newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=["title",
+                                                          "year",
+                                                          "rating",
+                                                          "poster_url",
+                                                          "imdb_id",
+                                                          "note"]
+                                        )
+                writer.writeheader()
 
 
 def main():
     """Main function to initialize and run the movie app."""
     parser = argparse.ArgumentParser(
         description="Start the Movie App with a specified storage "
-                    "file.")
-    parser.add_argument("storage_file",
-                        help="The storage file (JSON or CSV) to be "
-                             "used for storing movie data.")
+                    "file."
+    )
+    parser.add_argument(
+        "storage_file",
+        help="The storage file (JSON or CSV) to be used for "
+             "storing movie data."
+    )
 
     args = parser.parse_args()
     storage_file = os.path.join("data", args.storage_file)
-
+    create_file_if_not_exists(storage_file)
     _, file_extension = os.path.splitext(storage_file)
 
     if file_extension == ".json":
@@ -22,9 +46,8 @@ def main():
     elif file_extension == ".csv":
         storage = StorageCsv(storage_file)
     else:
-        print(
-            "Error: Unsupported file type. Please use a .json or "
-            ".csv file.")
+        print("Error: Unsupported file type. Please use a .json or "
+              ".csv file.")
         return
 
     app = MovieApp(storage)
